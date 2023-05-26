8a9f044db3c39b792fdd6bc6a6ef7ba0d29b44fb
Ducaxias <eduardo.caxias90@gmail.com>
2023-05-26 15:39:02 -0300

    :construction: feat(imports): imports e estilização

--- LagartoMacacoCrocodilo/src/components/Navbar.vue
+++ LagartoMacacoCrocodilo/src/components/Navbar.vue
@@ -4,9 +4,9 @@
             <h1 href="#!" class="logo">Clovis</h1>
             <ul class="navlinks">
                 <RouterLink to="/" class="link">Inicio</RouterLink>
-                <li class="link"><a href="#">Galeria</a></li>
-                <li class="link"><a href="#">Exposições</a></li>
-                <li class="link"><a href="#">Projetos</a></li>
+                <li class="link"><a href="#Galeria">Galeria</a></li>
+                <li class="link"><a href="#Exposições">Exposições</a></li>
+                <li class="link"><a href="#Workshops">Workshops</a></li>
                 <RouterLink to="/Login" class="link">Login</RouterLink>
             </ul>
         </nav>
@@ -19,3 +19,41 @@
         name: "Navbar"
     }
 </script>
+
+<style>
+.logo{
+    font-size: 35px;
+}
+
+.link, .link a{
+    color: var(--corprimaria);
+    font-size: 18px;
+    transition: .3s;
+    letter-spacing: 3px;
+}
+
+header{
+    width: 100%;
+    height: 70px;
+    position: fixed;
+    padding: 10px;
+    background-color: #fff;
+    border-bottom: 2px solid #000;
+}
+
+nav{
+    display: flex;
+    flex-direction: row;
+    gap: 53%;
+    width: auto;
+    align-items: center;
+}
+
+ul{
+    gap: 20px;
+    width: auto;
+    padding: 5px;
+    display: flex;
+    flex-direction: row;
+}
+</style>
--- LagartoMacacoCrocodilo/src/views/HomeView.vue
+++ LagartoMacacoCrocodilo/src/views/HomeView.vue
@@ -1,22 +1,80 @@
 <script setup lang="ts">
-
+import SaibaMais from "../components/SaibaMais.vue"
 </script>
 
 <template>
   <main>
-    <section class="Disclaimer">
-      <article>
-        <h1>Clovis</h1>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Pariatur eius ratione saepe fuga impedit 
-          similique! Unde veritatis corporis esse labore, quam aspernatur dolor quibusdam, aliquid vitae 
-          inventore maxime blanditiis non?</p> 
+    <section id="Inicio">
+      <article class="compose">
+        <div class="card">
+          <h1>Clovis</h1>
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Pariatur eius ratione saepe fuga impedit 
+            similique! Unde veritatis corporis esse labore, quam aspernatur dolor quibusdam, aliquid vitae 
+            inventore maxime blanditiis non?</p>
+        </div>
+        <div class="parallax"></div>
       </article>
     </section>
-    <section class="Galeria">
+    <section id="Galeria">
+      <h3>Galeria</h3>
+      <div class="Galeria">
+        <figure class="item_galeria">
+          <img src="" alt="">
+        </figure>
+        <figure class="item_galeria">
+          <img src="" alt="">
+        </figure>
+        <figure class="item_galeria">
+          <img src="" alt="" srcset="">
+        </figure>
+        <figure class="item_galeria">
+          <img src="" alt="">
+        </figure>
+        <figure class="item_galeria">
+          <img src="" alt="">
+        </figure>
+        <figure class="item_galeria">
+          <img src="" alt="">
+        </figure>
+      </div>
     </section>
-    <section class="Exposições">
+    <section id="Exposições">
+      <article>
+        <h3>Exposições</h3>
+        <p>A exposição Nova Era apresenta uma coleção diversificada de obras de arte de 
+          renomados artistas contemporâneos e talentosos artistas emergentes. Das pinturas aos esculturas, 
+          das instalações às fotografias, esta exposição oferece uma visão abrangente das diferentes formas 
+          de expressão artística.</p>
+        <div class="banner">
+          <img src="" alt="Banner da exposição">
+        </div>
+      </article>
     </section>
-    <section class="Projetos">
+    <section class="Workshops" id="Workshops">
+      <h3>Workshops</h3>
+      <article class="cardWorkshops">
+          <div class="card">
+            <h4 class="">A arte contemporânea na vida das pessoas</h4>
+            <p class="description">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Excepturi hic aliquid iste 
+              explicabo, culpa, atque voluptates tenetur dolorum nisi amet illum voluptatibus expedita nulla 
+              magni quis laborum necessitatibus eius voluptas.</p>
+            <SaibaMais/>
+          </div>
+          <div class="card">
+            <h4 class="">Esculturas em argila e madeira(entalhe)</h4>
+            <p class="description">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestiae adipisci 
+              deleniti, fugiat expedita asperiores ratione et quas aspernatur voluptatum alias, 
+              perspiciatis perferendis. Nesciunt iusto, ab nam labore deserunt fugiat necessitatibus!</p>
+            <SaibaMais/>
+          </div>
+          <div class="card">
+            <h4>Diferentes formas de arte e como elas agregam a sociedade</h4>
+            <p class="description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Error esse illo 
+              nam odio nihil id eum saepe incidunt! Molestias esse iusto odit assumenda repudiandae quasi
+               quos nesciunt atque possimus excepturi.</p>
+            <SaibaMais/>
+          </div>
+      </article>
     </section>
   </main>
 </template>
