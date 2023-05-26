636e1a48c33af69baae4e6f2d486643c114b2937
Ducaxias <eduardo.caxias90@gmail.com>
2023-05-26 08:47:57 -0300

    :sparkles: feat(conteudo): Adiciona conteudos a main

--- LagartoMacacoCrocodilo/src/views/HomeView.vue
+++ LagartoMacacoCrocodilo/src/views/HomeView.vue
@@ -1,9 +1,22 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <section class="Disclaimer">
+      <article>
+        <h1>Clovis</h1>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Pariatur eius ratione saepe fuga impedit 
+          similique! Unde veritatis corporis esse labore, quam aspernatur dolor quibusdam, aliquid vitae 
+          inventore maxime blanditiis non?</p> 
+      </article>
+    </section>
+    <section class="Galeria">
+    </section>
+    <section class="Exposições">
+    </section>
+    <section class="Projetos">
+    </section>
   </main>
 </template>
