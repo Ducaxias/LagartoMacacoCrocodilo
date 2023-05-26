fb411c050529fdb66b773917ec2642d1fbdef0bd
Ducaxias <eduardo.caxias90@gmail.com>
2023-05-26 15:34:51 -0300

    :sparkles: feat(card): Adiciona cards

---
+++ LagartoMacacoCrocodilo/src/components/SaibaMais.vue
@@ -0,0 +1,47 @@
+<template>
+    <a href="#" class="button">
+        <span>Saiba mais</span>
+    </a>
+</template>
+
+<script>
+    export default{
+        name: "button"
+    }
+</script>
+
+<style>
+    .button{
+        width: 200px;
+        height: 30px;
+        color: #fff;
+        transition: .2s;
+        border-radius: 10px;
+        background-color: #000;
+        border: #000 solid 2px;
+        display: block;
+        margin-left: 75px;
+    }
+
+    .button:hover{
+        color: black;
+        background-color: #fff;
+    }
+
+    span{
+        text-transform: uppercase;
+        position: relative;
+        font-size: 16px;
+        top: 2px;
+        left: 50px;
+    }
+
+    span::after{
+        content: '+';
+        display: inline;
+        width: 40px;
+        height: 40px;
+        margin-left: 30px;
+    }
+
+</style>
