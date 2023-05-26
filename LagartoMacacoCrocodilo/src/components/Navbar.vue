83eaa1bad2d00a7bb4600760eff3b5e2611312aa
Ducaxias <eduardo.caxias90@gmail.com>
2023-05-26 08:39:57 -0300

    :sparkles: feat(header): Adiciona Links de navegação

---
+++ LagartoMacacoCrocodilo/src/components/Navbar.vue
@@ -0,0 +1,21 @@
+<template>
+    <header>
+        <nav>
+            <h1 href="#!" class="logo">Clovis</h1>
+            <ul class="navlinks">
+                <RouterLink to="/" class="link">Inicio</RouterLink>
+                <li class="link"><a href="#">Galeria</a></li>
+                <li class="link"><a href="#">Exposições</a></li>
+                <li class="link"><a href="#">Projetos</a></li>
+                <RouterLink to="/Login" class="link">Login</RouterLink>
+            </ul>
+        </nav>
+    </header>
+
+</template>
+
+<script>
+    export default{
+        name: "Navbar"
+    }
+</script>
